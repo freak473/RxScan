49c6303fb0db7decb593ef359b92cdd13e112181
Ankit Jain <jainankit1997@gmail.com>
2026-07-23 15:30:38 +0530

    android: add Room + DataStore + KSP to the gradle build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -60,5 +61,9 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.datastore.preferences)
+    testImplementation(libs.junit)
     debugImplementation(libs.androidx.ui.tooling)
 }
